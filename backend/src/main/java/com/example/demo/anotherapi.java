25ba83f2cf11e61e0d9aa749b7757b2b99fd47c3
Kabilan Sivapragash <36630232+Kabilan35@users.noreply.github.com>
2019-04-22 20:20:00 +0530

    Add files via upload

--- backend/src/main/java/com/example/demo/anotherapi.java
+++ backend/src/main/java/com/example/demo/anotherapi.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 import org.openqa.selenium.By;
+import org.openqa.selenium.StaleElementReferenceException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -22,44 +23,71 @@ public class anotherapi {
 
       @RequestMapping(method = RequestMethod.POST, value="tags1")
       public ResponseEntity<?> getTags(@RequestBody model url){
-            System.setProperty("webdriver.chrome.driver","D:\\ChromeDriver\\chromedriver.exe" ); 
-            WebDriver driver=new ChromeDriver();  
-            driver.get(url.getUrl());
-            List<WebElement> s1= new ArrayList<WebElement>();
-            s1.addAll(driver.findElements(By.cssSelector("a")));
-            s1.addAll(driver.findElements(By.cssSelector("button")));
-            s1.addAll(driver.findElements(By.cssSelector("input")));
-            List<String> test = new ArrayList<String>();
-            xpath xp = new xpath();
-            Map<String,String> map1 = new HashMap<String,String>();
-            Map<String,String> map2 = new HashMap<String,String>();
-            ResponseEntity<model> response;
-            String current = "";
-            for(WebElement elm :s1 ) {
-                                                                                //test.add(xp.generateXpath(driver,elm));
-                 test.add(xp.generateXpath(elm,current));
-                 map2.put(elm.getText(),xp.generateXpath(elm,current));
-                 map1.put(elm.getText(),elm.getTagName());
-                                                                                
-                 if(elm.getTagName()!="input")
-                 {                                                           
-                  map1.put(elm.getText(),elm.getTagName());
-                 }                                                            
-                 if(elm.getAttribute("type")=="")
-                 {
-                                 map1.put(elm.getAttribute("name"),elm.getTagName());
-                 }
-                 else
-                 {
-                                 map1.put(elm.getAttribute("type"),elm.getTagName());
-                 }
-                 if(map1.containsKey(""))
-                 {
-                                 map1.remove("");
-                 }                                                          
-                                                                                
-                     }
-			            driver.close();
+            System.setProperty("webdriver.chrome.driver","C:\\Users\\Kabilan\\Documents\\chromedriver.exe" ); 
+        WebDriver driver=new ChromeDriver();  
+        driver.get("https://www.google.com/");
+        List<WebElement> list1= new ArrayList<WebElement>();	//for inputs, buttons and links
+        List<String> list2= new ArrayList<String>();    //for all links only
+        List<WebElement> list3= new ArrayList<WebElement>();	//for links from new pages
+        List<String> list4= new ArrayList<String>();
+        list1.addAll(driver.findElements(By.cssSelector("a")));
+        list1.addAll(driver.findElements(By.cssSelector("button")));
+        list1.addAll(driver.findElements(By.cssSelector("input")));
+        List<String> test = new ArrayList<String>();
+        xpath xp = new xpath();
+        Map<String,String> map1 = new HashMap<String,String>();
+        Map<String,String> map2 = new HashMap<String,String>();
+        String current = "";
+
+        for(WebElement elm :list1 ) {
+             //test.add(xp.generateXpath(driver,elm));
+             test.add(xp.generateXpath(elm,current));
+             list2.add(elm.getAttribute("href"));
+             
+             //looping through all pages
+             for(int i =0; i< list2.size(); ++i)
+             {
+            	 System.out.println("DONE");
+             	if(!list2.get(i).equals("javascript:void(0)") && !list2.get(i).equals(""))
+             	{	System.out.println(list2.get(i));
+             		driver.get(list2.get(i));}
+             	list4.add(list2.get(i));   //adding visited links to list
+             	list3.addAll(driver.findElements(By.cssSelector("a")));
+             	
+             	for(WebElement elm1 :list3)	
+             		{
+             		try{
+             			if(!list2.contains(elm1.getAttribute("href")));
+             			{list2.add(elm1.getAttribute("href"));}
+             			 map1.put(elm1.getText(),elm1.getTagName());
+             			 map2.put(elm1.getText(),xp.generateXpath(elm1,current));
+             			}
+             		catch(StaleElementReferenceException e) {}
+             			}
+             }
+             
+             
+             if(elm.getTagName()!="input")
+             {                                                           
+              map1.put(elm.getText(),elm.getTagName());
+             }                                                            
+             if(elm.getAttribute("type")=="")
+             {
+                             map1.put(elm.getAttribute("name"),elm.getTagName());
+             }
+             else
+             {
+                             map1.put(elm.getAttribute("type"),elm.getTagName());
+             }
+             if(map1.containsKey(""))
+             {
+                             map1.remove("");
+             }
+    }
+
+    System.out.println(map1);
+    System.out.println(map2);
+    driver.close();
 			            url.setElement(map1);
 			            url.setXpath(test);
 			            url.setElement1(map2);
--- backend/src/main/java/com/example/demo/xpath.java
+++ backend/src/main/java/com/example/demo/xpath.java
@@ -8,10 +8,7 @@ import org.openqa.selenium.WebElement;
 
 public class xpath{
                 public String generateXpath(WebElement childElement, String current){
-//public String generateXpath(WebDriver driver, WebElement ele){
-//            JavascriptExecutor js=(JavascriptExecutor) driver;
-//            return (String)js.executeScript("gPt=function(c){if(c.id!==''){return'id(\"'+c.id+'\")'}if(c===document.body){return c.tagName}var a=0;var e=c.parentNode.childNodes;for(var b=0;b<e.length;b++){var d=e[b];if(d===c){return gPt(c.parentNode)+'/'+c.tagName+'['+(a+1)+']'}if(d.nodeType===1&&d.tagName===c.tagName){a++}}};return gPt(arguments[0]).toLowerCase();", ele);
-//}
+
     String childTag = childElement.getTagName();
     if(childTag.equals("html")) {
         return "/html[1]"+current;
@@ -32,4 +29,8 @@ public class xpath{
     }
     return null;
 }
+                public String generateXpath(WebDriver driver, WebElement ele){
+                    JavascriptExecutor js=(JavascriptExecutor) driver;
+                    return (String)js.executeScript("gPt=function(c){if(c.id!==''){return'id(\"'+c.id+'\")'}if(c===document.body){return c.tagName}var a=0;var e=c.parentNode.childNodes;for(var b=0;b<e.length;b++){var d=e[b];if(d===c){return gPt(c.parentNode)+'/'+c.tagName+'['+(a+1)+']'}if(d.nodeType===1&&d.tagName===c.tagName){a++}}};return gPt(arguments[0]).toLowerCase();", ele);
+        }
 }
